459afc48707428ef0f04961caa8e9fd01a1bc5bc
Vadym Pylypchenko <vadym_pylyp@ukr.net>
2017-03-01 09:16:47 +0200

    5,6

---
+++ src/main/resources/db/6.sql
@@ -0,0 +1,6 @@
+# 6. Вычислить, среднюю ЗП программистов в проекте, который приносит наименьшую прибыль.
+
+SELECT projects.*,avg(developers.salary) FROM developers, projects
+  WHERE developers.project_id = projects.id
+        AND projects.id IN (SELECT id FROM projects
+            WHERE cost IN(SELECT min(projects.cost) FROM projects));
