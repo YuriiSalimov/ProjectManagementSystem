3a04464e407ef195ee2fd73b611e8458f6e71663
Vadym Pylypchenko <vadym_pylyp@ukr.net>
2017-03-01 21:59:09 +0200

    Developer Class

--- src/main/java/com/management/project/models/Developer.java
+++ src/main/java/com/management/project/models/Developer.java
@@ -1,7 +1,120 @@
 package com.management.project.models;
 
-/**
- * Created by Aleksey on 27.02.2017.
+import java.util.Collection;
+
+/*
+package com.management.project.models;
+
+import java.util.Collection;
+
+
+public class Developer {
+
+    private Long id;
+    private String name;
+    private Company company;
+    private Project project;
+    private int salary;
+    private Collection<Skill> skills;
+
+    public Developer(Long id, String name, Company company, Project project, int salary, Collection<Skill> skills) {
+        this.id = id;
+        this.name = name;
+        this.company = company;
+        this.project = project;
+        this.salary = salary;
+        this.skills = skills;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Collection<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(Collection<Skill> skills) {
+        this.skills = skills;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Developer developer = (Developer) o;
+
+        if (salary != developer.salary) return false;
+        if (!id.equals(developer.id)) return false;
+        if (!name.equals(developer.name)) return false;
+        if (company != null ? !company.equals(developer.company) : developer.company != null) return false;
+        if (project != null ? !project.equals(developer.project) : developer.project != null) return false;
+        return skills != null ? skills.equals(developer.skills) : developer.skills == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + name.hashCode();
+        result = 31 * result + (company != null ? company.hashCode() : 0);
+        result = 31 * result + (project != null ? project.hashCode() : 0);
+        result = 31 * result + salary;
+        result = 31 * result + (skills != null ? skills.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Developer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", company=" + company +
+                ", project=" + project +
+                ", salary=" + salary +
+                ", skills=" + skills +
+                '}';
+    }
+}
+
+
  */
 
     /* Class Developer*/
