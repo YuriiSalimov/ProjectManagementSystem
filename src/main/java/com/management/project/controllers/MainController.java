7d3131bf4b2f8f7da93475fc056ed71ec8ea1c76
Makhinich <slav.m82@gmail.com>
2017-03-08 10:03:55 +0200

    FactoryController

---
+++ src/main/java/com/management/project/controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.management.project.controllers;
+
+/**
+ * @author Slava Makhinich
+ */
+public class MainController extends AbstractController {
+    @Override
+    protected void action(int choice) {
+
+    }
+
+    @Override
+    protected void printMenu() {
+
+    }
+}
