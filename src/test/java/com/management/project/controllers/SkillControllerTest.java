e6e7137dabd2bd9d040a7c1e9744eccc8ae102e2
KateShatik <katrin.shatik@gmail.com>
2017-03-25 03:32:38 +0300

    SkillControllerTest

--- src/test/java/com/management/project/controllers/SkillControllerTest.java
+++ src/test/java/com/management/project/controllers/SkillControllerTest.java
@@ -1,38 +1,57 @@
 package com.management.project.controllers;
 
+import com.management.project.dao.CompanyDAO;
+import com.management.project.dao.GenericDAO;
+import com.management.project.dao.ProjectDAO;
+import com.management.project.dao.SkillDAO;
+import com.management.project.models.Company;
+import com.management.project.models.Developer;
+import com.management.project.models.Project;
 import com.management.project.models.Skill;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Катя on 19.03.2017.
  */
 public class SkillControllerTest extends AbstarctSkillControllerTest {
-    private Skill skill;
+    GenericDAO dao = mock(GenericDAO.class);
+    SkillDAO skillDAO = mock(SkillDAO.class);
 
-    private AbstractModelController abstractModelController;
+    SkillController skillController = new SkillController(skillDAO);
     @Test
-    public void printMenu() {
-        String menu = "ACTIONS WITH SKILLS:";
-        SkillController skillController = null;
+    public void printMenu() throws Exception {
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
         skillController.printMenu();
-        assertTrue(menu.contains(output.toString()));
-    }
+        assertTrue(byteArrayOutputStream.toString().contains("ACTIONS WITH SKILLS:"));
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
 
+    }
 
     @Test
-    public void getNewModel(){
-        String testName = new Scanner(System.in).nextLine();
-        Skill testSkill = new Skill();
-        testSkill.setName(testName);
-        assertNotNull(testSkill);
-        assertEquals(testSkill.getName(), testName);
+    public void getNewModel() throws Exception {
+
+        Skill skill = new Skill(-100, "test skill");
+        when(skillDAO.findById(1l)).thenReturn(skill).thenReturn(null);
+        System.setIn(new Always1InputStream());
+
+        Skill skillFromGetNewModel = skillController.getNewModel();
+        assertEquals(skillFromGetNewModel, skill);
+
+
     }
-
-
 }
