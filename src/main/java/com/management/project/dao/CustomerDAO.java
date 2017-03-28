4c39089782f48060d60f8d650699d9f4197d375d
Makhinich <slav.m82@gmail.com>
2017-03-28 13:43:10 +0300

    CustomerDAO documentation

--- src/main/java/com/management/project/dao/CustomerDAO.java
+++ src/main/java/com/management/project/dao/CustomerDAO.java
@@ -3,7 +3,7 @@ package com.management.project.dao;
 import com.management.project.models.Customer;
 
 /**
- * The interface extends methods for working with data base and entity from GenericDAO
+ * The interface extends methods from GenericDAO for working with data base and entity
  * The interface will be implemented by DAO classes that will work with Customer entity
  *
  * @author Perevoznyk Pavel
